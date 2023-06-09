c7d0cee9e3e17e63798d19618c597a59afd5f338
Caio Ceretta <caioceretta@gmail.com>
2023-06-09 16:53:44 -0300

    Added the about page

---
+++ imdb/src/app/about/page.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="mx-auto max-w-6xl space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to our movie database website! We are a team of passionate movie
+        enthusiasts
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nulla,
+        distinctio amet tenetur nemo consequatur explicabo ad consequuntur
+        voluptatibus voluptatem.
+      </p>
+      <p>
+        Aut nihil dicta laudantium tenetur, inventore architecto corrupti error
+        reiciendis eos.
+      </p>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Consequuntur,
+        exercitationem accusantium dolorum at explicabo voluptatum facere
+        consectetur doloremque nobis temporibus perspiciatis, voluptate earum
+        vero enim! Sint cum officiis eius excepturi.
+      </p>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus
+        facere reprehenderit distinctio ducimus dolorem eum omnis unde quaerat
+        dolore? Eius impedit facilis reprehenderit libero minus veritatis ut,
+        iusto autem nostrum?
+      </p>
+    </div>
+  )
+}
