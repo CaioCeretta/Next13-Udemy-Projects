c2100b2e69cdd86add7663dfa3ddd2e8f53b3246
Caio Ceretta <caioceretta@gmail.com>
2023-06-16 22:50:33 -0300

    Started creating the web page (Google)

--- google/src/app/search/web/page.tsx
+++ google/src/app/search/web/page.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+type Params = {
+  params: {
+    searchTerm: string
+  }
+}
 
-export default function WebSearchPage() {
-  return <div>Web</div>
+interface Result {
+  title: string
+}
+
+interface Results {
+  items: Result[]
+}
+
+export default async function WebSearchPage({
+  params: { searchTerm },
+}: Params) {
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}`,
+  )
+
+  console.log(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchTerm}`,
+  )
+
+  const data = (await response.json()) as Results
+
+  const results = data.items
+
+  return (
+    <>
+      {results &&
+        results.map((result) => <h1 key={result.title}>{result.title}</h1>)}
+    </>
+  )
 }
