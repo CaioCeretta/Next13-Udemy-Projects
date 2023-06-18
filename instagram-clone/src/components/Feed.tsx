518b251dcdcb4242046def2738c4a6848c859cad
Caio Ceretta <caioceretta@gmail.com>
2023-06-17 23:57:29 -0300

    Made the section responsive

--- instagram-clone/src/components/Feed.tsx
+++ instagram-clone/src/components/Feed.tsx
@@ -3,14 +3,14 @@ import Stories from './Stories'
 
 export default function Feed() {
   return (
-    <main>
-      <section>
+    <main className="mx-auto grid grid-cols-1 md:max-w-6xl md:grid-cols-3">
+      <section className="md:col-span-2">
         {/* stories */}
         <Stories />
         {/* posts */}
         <Posts />
       </section>
-      <section>
+      <section className="hidden md:col-span-1 md:inline-grid">
         {/* Mini Profile */}
         {/* Suggestions */}
       </section>
